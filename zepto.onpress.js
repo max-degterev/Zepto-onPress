6afd4ca67c8ec792ddbb5aa49fa90a4e19cc43c6
Max Degterev <me@maxdegterev.name>
2012-01-26 21:25:23 +0400

    Saner code

--- zepto.onpress.js
+++ zepto.onpress.js
@@ -8,30 +8,31 @@
     
     var touches = {},
         doc = $(document),
-        hasMoved = false;
-        
-    doc.on('touchstart', function(e) {
-        touches.x = e.touches[0].pageX;
-        touches.y = e.touches[0].pageY;
-        hasMoved = false;
-    });
-    doc.on('touchmove', function(e) {
-        if(Math.abs(e.touches[0].pageX - touches.x) > 10 || Math.abs(e.touches[0].pageY - touches.y) > 10) {
-            hasMoved = true;
-        }
-    });
-
-    var handler = function(e) {
-        $(e.target).trigger('press', e);
-    };
-        
+        hasMoved = false,
+        target;
+    
     if ($.os.touch) {
-        doc.on('touchend', function(e) { hasMoved || handler(e); });
+        doc.on('touchstart', function(e) {
+            touches.x = e.touches[0].pageX;
+            touches.y = e.touches[0].pageY;
+            hasMoved = false;
+            target = e.target;
+        });
+        doc.on('touchmove', function(e) {
+            if(Math.abs(e.touches[0].pageX - touches.x) > 10 || Math.abs(e.touches[0].pageY - touches.y) > 10) {
+                hasMoved = true;
+            }
+        });
+        doc.on('touchend', function(e) {
+            ((target === e.target) && !hasMoved) && $(e.target).trigger('press', e);
+        });
     }
     else {
-        doc.on('click', handler);
+        doc.on('click', function(e) {
+            $(e.target).trigger('press', e);
+        });
     }
-    
+
     $.fn.onpress = function(callb) {
         this.on('press', callb);
     };
