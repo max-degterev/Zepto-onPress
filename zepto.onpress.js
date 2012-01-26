f50b3355ce3b6add39dcc605f7f2c7935b63e1c0
Max Degterev <me@maxdegterev.name>
2012-01-26 16:11:15 +0400

    Removed clientX/Y

--- zepto.onpress.js
+++ zepto.onpress.js
@@ -1,39 +1,38 @@
 /* Author:
-	Max Degterev @suprMax
+    Max Degterev @suprMax
 */
 
 ;(function($) {
-	// Zepto touch device detection
-	$.os.touch = !(typeof window.ontouchstart === 'undefined');
-	
-	var touches = {},
-		doc = $(document),
-		hasMoved = false;
-		
-	doc.on('touchstart', function(e) {
-	    touches.startX = e.touches[0].clientX;
-	    touches.startY = e.touches[0].clientY;
-		hasMoved = false;
-	});
-	doc.on('touchmove', function(e) {
-	    if(Math.abs(e.touches[0].clientX - touches.startX) > 10 || 
-	       Math.abs(e.touches[0].clientY - touches.startY) > 10) {
-	        hasMoved = true;
-	    }
-	});
+    // Zepto touch device detection
+    $.os.touch = !(typeof window.ontouchstart === 'undefined');
+    
+    var touches = {},
+        doc = $(document),
+        hasMoved = false;
+        
+    doc.on('touchstart', function(e) {
+        touches.x = e.touches[0].pageX;
+        touches.y = e.touches[0].pageY;
+        hasMoved = false;
+    });
+    doc.on('touchmove', function(e) {
+        if(Math.abs(e.touches[0].pageX - touches.x) > 10 || Math.abs(e.touches[0].pageY - touches.y) > 10) {
+            hasMoved = true;
+        }
+    });
 
-	var handler = function(e) {
-		$(e.target).trigger('press', e);
-	};
-		
-	if ($.os.touch) {
-		doc.on('touchend', function(e) { hasMoved || handler(e); });
-	}
-	else {
-		doc.on('click', handler);
-	}
-	
-	$.fn.onpress = function(callb) {
-		this.on('press', callb);
-	};
+    var handler = function(e) {
+        $(e.target).trigger('press', e);
+    };
+        
+    if ($.os.touch) {
+        doc.on('touchend', function(e) { hasMoved || handler(e); });
+    }
+    else {
+        doc.on('click', handler);
+    }
+    
+    $.fn.onpress = function(callb) {
+        this.on('press', callb);
+    };
 })(Zepto);
